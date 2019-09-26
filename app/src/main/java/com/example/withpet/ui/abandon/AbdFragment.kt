5073caeef0360b7d9cd2cc9a318cebc652aa11e0
정서윤 <seoyoon630@neobns.com>
2019-09-26 18:05:59 +0900

    1. 유기견 todo 작성

--- app/src/main/java/com/example/withpet/ui/abandon/AbdFragment.kt
+++ app/src/main/java/com/example/withpet/ui/abandon/AbdFragment.kt
@@ -10,6 +10,16 @@ import com.example.withpet.core.BaseFragment
 import com.example.withpet.databinding.AbdFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
+/**
+ * todo
+ * 1. 서울시 시군구 번호 받아오기 api 추가
+ * 2. 선택한 시군구에 있는 유기견 api 추가
+ * 3. 유기견 api 페이징 처리 추가
+ * 4. 유기견 데이터 리스트로 뿌리기 추가
+ * 5. 유기견 상세 페이지 추가
+ * 6. 유기견 기사? 정보?? 페이지 추가
+ * 7.  유기견 기사? 정보?? 상세 페이지 추가
+ */
 class AbdFragment : BaseFragment() {
 
     lateinit var binding: AbdFragmentBinding
