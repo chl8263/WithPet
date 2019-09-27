c4b6286a53507dcb2bcab5a94f02afa7cf188cb2
정서윤 <seoyoon630@neobns.com>
2019-09-27 18:18:04 +0900

    1. 유기견 작업 중

--- app/src/main/java/com/example/withpet/ui/abandon/AbdFragment.kt
+++ app/src/main/java/com/example/withpet/ui/abandon/AbdFragment.kt
@@ -1,5 +1,6 @@
 package com.example.withpet.ui.abandon
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,22 +9,25 @@ import androidx.databinding.DataBindingUtil
 import com.example.withpet.R
 import com.example.withpet.core.BaseFragment
 import com.example.withpet.databinding.AbdFragmentBinding
+import androidx.lifecycle.Observer
+import com.example.withpet.util.Log
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 /**
  * todo
- * 1. 서울시 시군구 번호 받아오기 api 추가
- * 2. 선택한 시군구에 있는 유기견 api 추가
+ * 1. 서울시 시군구 번호 받아오기 api 추가     완료(enum으로 변경)
+ * 2. 선택한 시군구에 있는 유기견 api 추가     완료(enum으로 변경)
  * 3. 유기견 api 페이징 처리 추가
  * 4. 유기견 데이터 리스트로 뿌리기 추가
  * 5. 유기견 상세 페이지 추가
  * 6. 유기견 기사? 정보?? 페이지 추가
- * 7.  유기견 기사? 정보?? 상세 페이지 추가
+ * 7. 유기견 기사? 정보?? 상세 페이지 추가
  */
 class AbdFragment : BaseFragment() {
 
     lateinit var binding: AbdFragmentBinding
-    val viewModel: AbdViewModel by viewModel()
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.abd_fragment, container, false)
@@ -32,11 +36,15 @@ class AbdFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        observeLiveData()
+
+        onLoadOnce()
     }
 
-    private fun observeLiveData() {
-        viewModel.getSido()
+    private fun onLoadOnce() {
+
+        binding.pet.setOnClickListener {
+            startActivity(Intent(mContext, AbdListActivity::class.java))
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/withpet/ui/abandon/usecase/AbdUseCase.kt
+++ app/src/main/java/com/example/withpet/ui/abandon/usecase/AbdUseCase.kt
@@ -2,20 +2,45 @@ package com.example.withpet.ui.abandon.usecase
 
 import android.content.Context
 import com.example.withpet.R
-import com.example.withpet.util.Auth
-import com.example.withpet.util.Log
-import com.example.withpet.vo.diary.DiaryDTO
-import com.google.common.base.Utf8
-import com.google.firebase.firestore.FirebaseFirestore
+import com.example.withpet.ui.abandon.enums.eSido
+import com.example.withpet.ui.abandon.enums.eSigungu
+import com.example.withpet.ui.abandon.enums.eState
+import com.example.withpet.ui.abandon.enums.eUpkind
+import com.example.withpet.vo.abandon.AbandonAnimalDTO
 import io.reactivex.Single
 import retrofit2.Call
-import java.net.URLEncoder
 
 interface AbdUseCase {
     fun getSido(): Call<String>
+
+    fun getList(
+        eSigungu: eSigungu,
+        pageNo: Int
+    ): Single<AbandonAnimalDTO>
 }
 
-class AbdUseCaseImpl(private val context: Context, private val abdDataSource: AbdDataSource) : AbdUseCase {
+class AbdUseCaseImpl(private val context: Context, private val abdDataSource: AbdDataSource) :
+    AbdUseCase {
 
-    override fun getSido(): Call<String> = abdDataSource.getSido(context.getString(R.string.service_key))
+    private val serviceKey = context.getString(R.string.service_key)
+
+    override fun getSido(): Call<String> = abdDataSource.getSido(serviceKey)
+
+    override fun getList(
+        eSigungu: eSigungu,
+        pageNo: Int
+    ): Single<AbandonAnimalDTO> = abdDataSource.getList(
+        serviceKey,
+        "20140601",
+        "20190601",
+        eUpkind.개.code,
+        eSido.서울특별시.code,
+        null,/*org_cd*/
+        eState.전체.param,
+        pageNo,
+        100
+    )
+
 }
+
+
