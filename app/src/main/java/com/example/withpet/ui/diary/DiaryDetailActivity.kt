d51dea462544d9bdadac3117d4d74a33c27ca2a3
상혁 <jgps3505@gmail.com>
2019-09-29 12:46:45 +0900

    성장일기 상세화면

--- app/src/main/java/com/example/withpet/ui/diary/DiaryDetailActivity.kt
+++ app/src/main/java/com/example/withpet/ui/diary/DiaryDetailActivity.kt
@@ -1,8 +1,11 @@
 package com.example.withpet.ui.diary
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.withpet.R
 import com.example.withpet.core.BaseActivity
 import com.example.withpet.databinding.ActivityDiaryDetailBinding
+import com.example.withpet.vo.diary.DiaryDTO
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class DiaryDetailActivity : BaseActivity() {
@@ -12,6 +15,30 @@ class DiaryDetailActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bb = DataBindingUtil.setContentView(mActivity, R.layout.activity_diary_detail)
+        bb.vm = vm
+        onParseExtra()
+    }
 
+    private fun onParseExtra() {
+        try {
+            intent.getSerializableExtra(EXTRA.DIARY_DTO)?.let {
+                val dto = it as DiaryDTO
+                vm.init(dto)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    fun onLoadOnce() {
+
+    }
+
+
+    interface EXTRA {
+        companion object {
+            const val DIARY_DTO = "DIARY_DTO"
+        }
     }
 }
--- app/src/main/java/com/example/withpet/ui/diary/DiaryDetailViewModel.kt
+++ app/src/main/java/com/example/withpet/ui/diary/DiaryDetailViewModel.kt
@@ -3,6 +3,7 @@ package com.example.withpet.ui.diary
 import androidx.databinding.ObservableField
 import com.example.withpet.core.BaseViewModel
 import com.example.withpet.ui.diary.usecase.DiaryUseCase
+import com.example.withpet.vo.diary.DiaryDTO
 
 class DiaryDetailViewModel(val diaryUseCase: DiaryUseCase) : BaseViewModel() {
 
@@ -11,8 +12,10 @@ class DiaryDetailViewModel(val diaryUseCase: DiaryUseCase) : BaseViewModel() {
     val date = ObservableField<String>()
 
 
-    fun init() {
-
+    fun init(diaryDTO: DiaryDTO) {
+        content.set(diaryDTO.content)
+        imageUrl.set(diaryDTO.imageUrl)
+        date.set(diaryDTO.date)
     }
 
 }
